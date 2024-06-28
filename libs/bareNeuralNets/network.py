37a78b2b6248b4ef310b4c987c3245b2f6e54870
Obiwan <aaditya@Aaditya>
2024-06-29 00:37:12 +0530

    cleaning up the debug prints and comments

--- libs/bareNeuralNets/network.py
+++ libs/bareNeuralNets/network.py
@@ -23,7 +23,6 @@ class NNParamaters:
     
     def relu_derivative(x):
       y = (x > 0) * 1
-      # PrettyPrint.matrix(y)
       return y
     
   class LossFunctions:
@@ -75,73 +74,23 @@ class NeuralNetwork:
   def backpropagation(self, learning_constant, zs, activations, current_delta, current_layer, delta_ws, delta_bs):
     if current_layer == 0:
       return
-    
-    if VERBOSE_DEBUG:
-      print('|'*100)
-      print(f'running backprop for layer {current_layer}, learning constant = {learning_constant}')
-      print(f'Z[{current_layer-1}] shape: {zs[current_layer-1].shape}')
-      PrettyPrint.matrix(zs[current_layer-1])
-      print(f'activation[{current_layer}] shape: {activations[current_layer].shape}')
-      PrettyPrint.matrix(activations[current_layer])
-      print(f'delta_ws[{current_layer-1}] shape: {delta_ws[current_layer-1].shape}')
-      PrettyPrint.matrix(delta_ws[current_layer-1])
-      print(f'delta_bs[{current_layer-1}] shape: {delta_bs[current_layer-1].shape}')
-      PrettyPrint.matrix(delta_bs[current_layer-1])
-    
+
     delta_w = learning_constant * np.matmul(current_delta, activations[current_layer-1].T)
     delta_b = learning_constant * current_delta
     delta_ws[current_layer-1] += delta_w
     delta_bs[current_layer-1] += delta_b
-    
-    if VERBOSE_DEBUG:
-      print(f'delta_w was calculated by multiplying the below two matrices (current_delta: {current_delta.shape} and activations[{current_layer-1}].T: {activations[current_layer-1].T.shape}) multiplied by {learning_constant}:')  
-      PrettyPrint.matrix(current_delta)
-      PrettyPrint.matrix(activations[current_layer-1].T)
-      print(f'delta_w shape: {delta_w.shape}')
-      PrettyPrint.matrix(delta_w)
-      print(f'delta_b was calculated by multiplying the current_delta: {current_delta.shape} by {learning_constant}:')
-      PrettyPrint.matrix(delta_b)
-      
-      print(f'change in weights: ')
-      PrettyPrint.matrix(delta_w)
-      print(f'delta_ws[{current_layer-1}] shape: {delta_ws[current_layer-1].shape}')
-      PrettyPrint.matrix(delta_ws[current_layer-1])
-      print(f'delta_bs[{current_layer-1}] shape: {delta_bs[current_layer-1].shape}')
-      PrettyPrint.matrix(delta_bs[current_layer-1])
-      
-      print(f'Weights[{current_layer-1}].T shape: {self.weights[current_layer-1].T.shape}')
-      PrettyPrint.matrix(self.weights[current_layer-1].T)
-      print(f'Zs[{current_layer-1}] shape: {zs[current_layer-1].shape}')
-      PrettyPrint.matrix(zs[current_layer-1])
-      print(f'Activation Function Derivative of Zs[{current_layer-1}] shape: {self.activation_function_derivative(zs[current_layer-1]).shape}')
-      PrettyPrint.matrix(self.activation_function_derivative(zs[current_layer-1]))
-      
     previous_delta = np.matmul(self.weights[current_layer-1].T, current_delta) * self.activation_function_derivative(zs[current_layer-1])
-    
-    if VERBOSE_DEBUG:
-      print(f'Previous delta calulated by multiplying the below two matrices (weights[{current_layer-1}].T: {self.weights[current_layer-1].T.shape}'
-            f'and current_delta: {current_delta.shape}):')
-      
-      PrettyPrint.matrix(self.weights[current_layer-1].T)
-      PrettyPrint.matrix(current_delta)
-      print(f'Previous delta (shape: {previous_delta.shape}):')
-      PrettyPrint.matrix(previous_delta)
-      
-      print('|'*100)
-      
     self.backpropagation(learning_constant, zs, activations, previous_delta, current_layer-1, delta_ws, delta_bs)
 
   
   def train_minibatch(self, training_data, labels, epochs=1000):
     n = len(training_data)
-    # print(f'Length of training data: {n}')
     learning_constant = self.learning_rate / n
 
     while epochs > 0:
       delta_ws = [np.zeros(w.shape) for w in self.weights]
       delta_bs = [np.zeros(b.shape) for b in self.biases]
 
-      # print(f'Training Details for Epoch #{epochs}', '='*100)
       for datum, label in zip(training_data, labels):
         if (len(datum.shape) == 1):
           curr_datum = datum.reshape(datum.shape+(1,))
@@ -150,64 +99,15 @@ class NeuralNetwork:
           curr_datum = datum
           curr_label = label
         
-        zs, activations = self.feedforward(curr_datum)
-        if VERBOSE_DEBUG:
-          print(f'Zs: {len(zs)}')
-          for i in range(len(zs)):
-            print(f'Z[{i}] shape: {zs[i].shape}')
-            PrettyPrint.matrix(zs[i])
-          print(f'Activations: {len(activations)}')
-          for i in range(len(activations)):
-            print(f'Activation[{i}] shape: {activations[i].shape}')
-            PrettyPrint.matrix(activations[i])
-            
+        zs, activations = self.feedforward(curr_datum)    
         last_delta = self.cost_function_derivative(activations[-1], curr_label) * self.activation_function_derivative(zs[-1])
-        
-        if NON_VERBOSE_DEBUG:
-          print(f'Current Datum: {curr_datum.shape}')
-          PrettyPrint.matrix(curr_datum)
-          print(f'Current Label: {curr_label.shape}')
-          PrettyPrint.matrix(curr_label)
-          # print(f'last activation shape: {self.feedforward(curr_datum)[1][-1].shape}')
-          # PrettyPrint.matrix(activations[-1])
-          # print(f'last delta shape: {last_delta.shape}')
-          # PrettyPrint.matrix(last_delta)
-          # print(f'last z shape: {zs[-1].shape}')
-          # PrettyPrint.matrix(zs[-1])
-          for i in range(self.layers-1):
-            print(f'init delta weight #{i} ')
-            PrettyPrint.matrix(delta_ws[i])
-            print(f'init delta bias #{i} ')
-            PrettyPrint.matrix(delta_bs[i])
-            
         self.backpropagation(learning_constant, zs, activations, last_delta, self.layers-1, delta_ws, delta_bs)
-        if VERBOSE_DEBUG:
-          break
-          pass
-        
-      if NON_VERBOSE_DEBUG_2:
-        # print('Current Datum:')
-        # PrettyPrint.matrix(curr_datum)
-        # print('Current Label:')
-        # PrettyPrint.matrix(curr_label)
-        # print(f'last activation shape: {self.feedforward(curr_datum)[1][-1].shape}')
-        # PrettyPrint.matrix(activations[-1])
-        # print(f'last delta shape: {last_delta.shape}')
-        # PrettyPrint.matrix(last_delta)
-        # print(f'last z shape: {zs[-1].shape}')
-        # PrettyPrint.matrix(zs[-1])
-        for i in range(self.layers-1):
-          print(f'Changed delta weight #{i} ')
-          PrettyPrint.matrix(delta_ws[i])
-          print(f'CHanged delta bias #{i} ')
-          PrettyPrint.matrix(delta_bs[i])
-            
+          
       for i in range(self.layers-1):
         self.weights[i] -= delta_ws[i]
         self.biases[i] -= delta_bs[i]
 
       epochs -= 1
-      # print(f'End Epoch #{epochs}', '='*100)
 
   def predict(self, data):
     if NON_VERBOSE_DEBUG:
--- run.py
+++ run.py
@@ -3,10 +3,8 @@ from libs.prettyPrint.printMatrices import PrettyPrint
 from libs.mnist.loader import load_data_wrapper
 import numpy as np
 
-# training_data = np.array([[1, 0], [0, 1], [1, 1], [0, 0]])
-# training_labels = np.array([[1, 1, 0, 0]]).T
 training_data, validation_data, test_data = load_data_wrapper()
-# (data, training_labels) = list(training_data)
+
 training_data = np.array(list(training_data), dtype=object)
 validation_data = np.array(list(validation_data), dtype=object)
 test_data = np.array(list(test_data), dtype=object)
@@ -15,51 +13,4 @@ training_data, training_labels = training_data[:, 0], training_data[:, 1]
 validation_data, validation_labels = validation_data[:, 0], validation_data[:, 1]
 test_data, test_labels = test_data[:, 0], test_data[:, 1]
 
-print(test_labels)
-# print(data[0][0].shape)
-# print(training_data[0][1])
-# print(training_labels.shape)
-# PrettyPrint.matrix(training_data)
-# PrettyPrint.matrix(training_labels)
-
-# input_data = np.array([1, 1]).reshape(2, 1)
-# neural_network = NeuralNetwork([2, 3, 1])
-
-# for i, w in enumerate(neural_network.weights):
-#   print(f'weight #{i}')
-#   PrettyPrint.matrix(w)
-  
-# for i, b in enumerate(neural_network.biases):
-#   print(f'bias #{i}')
-#   PrettyPrint.matrix(b)
-
-# print('Initial Predictions:')
-# print('0, 0')
-# PrettyPrint.matrix(neural_network.predict(np.array([0, 0]).reshape(2, 1)), print_name=False)
-# print('0, 1')
-# PrettyPrint.matrix(neural_network.predict(np.array([0, 1]).reshape(2, 1)), print_name=False)
-# print('1, 0')
-# PrettyPrint.matrix(neural_network.predict(np.array([1, 0]).reshape(2, 1)), print_name=False)
-# print('1, 1')
-# PrettyPrint.matrix(neural_network.predict(np.array([1, 1]).reshape(2, 1)), print_name=False)
-
-# # for i in range(1):
-# neural_network.train_minibatch(training_data, training_labels, epochs=1000)
-
-# print('Post Training Predictions:')
-# print('0, 0')
-# PrettyPrint.matrix(neural_network.predict(np.array([0, 0]).reshape(2, 1)), print_name=False)
-# print('0, 1')
-# PrettyPrint.matrix(neural_network.predict(np.array([0, 1]).reshape(2, 1)), print_name=False)
-# print('1, 0')
-# PrettyPrint.matrix(neural_network.predict(np.array([1, 0]).reshape(2, 1)), print_name=False)
-# print('1, 1')
-# PrettyPrint.matrix(neural_network.predict(np.array([1, 1]).reshape(2, 1)), print_name=False)
-
-# # zs, activations = neural_network.feedforward(input_data)
-
-# PrettyPrint.matrix(input_data)
-
-# # for i, a in enumerate(activations):
-# #   print(f'activation #{i} ', end='')
-# #   PrettyPrint.matrix(a)
+neural_network = NeuralNetwork(structure=[784, 30, 10])
